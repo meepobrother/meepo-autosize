16a2f1267e53e0e1d7aec45cdc91d8678b9a12a6
15138730370 <1037483576@qq.com>
2017-12-22 17:11:44 +0800

    save to back

--- src/app/autosize/autosize.ts
+++ src/app/autosize/autosize.ts
@@ -1,12 +1,19 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+declare const require: any;
+let autosize = require('autosize');
 @Component({
     selector: 'autosize',
-    templateUrl: 'autosize.html'
+    templateUrl: './autosize.html',
+    styleUrls: [
+        './autosize.scss'
+    ]
 })
 
 export class AutoSizeComponent implements OnInit {
+    @ViewChild('textarea') textarea: ElementRef;
     constructor() { }
 
-    ngOnInit() { }
+    ngOnInit() { 
+        autosize(this.textarea.nativeElement)
+    }
 }
